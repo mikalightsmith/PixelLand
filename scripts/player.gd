32a46102d2a10d38abd4c0d7e45823d6ad30fd4e
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2024-06-20 13:23:44 -0700

    direction + crouch

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,11 @@ extends CharacterBody2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var is_crouching = false
+
+func _process(delta):
+	print(is_crouching)
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -25,8 +30,11 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 		
 	if direction != 0:
-		sprite.flip_h = (direction == -1)
-
+		set_direction(direction)
+	
+	if Input.is_action_just_pressed("crouch"):
+		toggle_crouch()
+		
 	move_and_slide()
 
 	update_animations(direction)
@@ -37,11 +45,27 @@ func _physics_process(delta):
 func update_animations(direction):
 	if is_on_floor():
 		if direction == 0:
-			ap.play("idle")
+			if is_crouching:
+				ap.play("crouch")
+			else:
+				ap.play("idle")
 		else:
-			ap.play("run")
+			if is_crouching:
+				ap.play("slide")
+			else:
+				ap.play("run")
 	else:
 		if velocity.y < 0:
 			ap.play("jump")
 		elif velocity.y > 0:
 			ap.play("fall")
+
+func set_direction(direction):
+	sprite.flip_h = (direction == -1)
+	sprite.position.x = direction * 7
+
+func toggle_crouch():
+	if is_crouching:
+		is_crouching = false
+	else:
+		is_crouching = true
