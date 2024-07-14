b29aaf827dcb936afe4f4105eabf5eebba932c9f
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2024-07-13 18:47:31 -0700

    adding attack animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,22 @@
 extends CharacterBody2D
 
 
-@export var speed = 400.0
-@export var jump_force = -400.0
+@export var speed = 150.0
+@export var jump_force = -300.0
 
 @onready var ap = $AnimationPlayer
 @onready var sprite = $Sprite2D
 @onready var cshape = $CollisionShape2D
 @onready var crouch_raycast_1 = $crouch_raycast_1
 @onready var crouch_raycast_2 = $crouch_raycast_2
+@onready var attack_timer = $AttackTimer
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var is_crouching = false
 var stuck_under_object = false
-
+var is_attacking = false
+ 
 var standing_cshape = preload("res://resources/warrior_standing_cshape.tres")
 var crouching_cshape = preload("res://resources/warrior_crouching_cshape.tres")
 
@@ -27,6 +29,13 @@ func _physics_process(delta):
 		if velocity.y > 1000:
 			velocity.y = 1000
 
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true
+		attack_timer.start()
+		
+	if is_attacking && attack_timer.is_stopped():
+		is_attacking = false
+	
 	if Input.is_action_just_pressed("jump"): # and is_on_floor():
 		velocity.y = jump_force
 
@@ -60,7 +69,9 @@ func above_head_is_empty() -> bool:
 	return not crouch_raycast_1.is_colliding() and not crouch_raycast_2.is_colliding()
 	
 func update_animations(direction):
-	if is_on_floor():
+	if is_attacking:
+		ap.play("attack")
+	elif is_on_floor():
 		if direction == 0:
 			if is_crouching:
 				ap.play("crouch")
@@ -80,8 +91,6 @@ func update_animations(direction):
 				ap.play("jump")
 			elif velocity.y > 0:
 				ap.play("fall")
-			
-			
 
 func set_direction(direction):
 	sprite.flip_h = (direction == -1)
@@ -92,7 +101,7 @@ func crouch():
 		return
 	is_crouching = true
 	cshape.shape = crouching_cshape
-	cshape.position.y = -10
+	cshape.position.y = -8
 
 func stand():
 	if not is_crouching:
